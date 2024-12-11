96ff3e8e1653c4d9db99fcecfd99f4d1a3828b6b
AndreBarros1 <andrelrcb778@gmail.com>
2024-12-11 20:36:27 -0300

    1.0

--- db/firebase.js
+++ db/firebase.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
 const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgjRV8i-82q8oFdwTO3i7RrDAnN3ZvS4M",
-    authDomain: "aula-firebase-ea011.firebaseapp.com",
-    projectId: "aula-firebase-ea011",
-    storageBucket: "aula-firebase-ea011.appspot.com",
-    messagingSenderId: "235575060905",
-    appId: "1:235575060905:web:aee0704af985f7da7afcb8"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   };
 
 const app = initializeApp(firebaseConfig);
-
 
 const db = getFirestore(app);
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 
 const server = express();
 
+server.use(cors()); 
 server.use(bodyParser.json());
 
 module.exports = server;
